089d6621c43d48a6263e1c094eac69392f4606ea
richard1230 <yongjinhu1230@gmail.com>
2020-12-08 21:51:08 +0800

    useReduce 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React ,{useState}from 'react';
+import React, {useState, useReducer} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = ()=>{
- const [n,setN] = useState(0)
-    const onClick = ()=>{
-     //这里传的是一个操作，不会产生之前的那个问题
-        setN(n=>n+1)
-        setN(n=>n+1)
+//1.创建初始值
+const initial = {
+    n: 0
+}
 
+//2.创建所有操作
+const reducer = (state, action) => {
+    if (action.type === 'add') {
+        return (
+            {n: state.n + action.number}
+        )
+    } else if (action.type === 'multi') {
+        return (
+            {
+                n: state.n * 2
+            }
+        )
+    } else {
+        throw new Error('unknow type')
     }
-    return(
+}
+
+const App = () => {
+    //3.state保存初始状态，可以读；dispatch可以写
+    const [state, dispatch] = useReducer(reducer, initial)
+    //从初始状态里面取值
+    const {n} = state
+    const onClick = () => {
+        //4.调用写
+        dispatch({type: 'add', number: 1})
+    }
+    const onClick2 = () => {
+        dispatch({type: 'add', number: 2})
+    }
+
+    return (
         <div className="App">
-            <h1>n：{n}</h1>
-            <button onClick={onClick}>Click</button>
+            <h1>n:{n}</h1>
+            <button onClick={onClick}>+1</button>
+            <button onClick={onClick2}>+2</button>
         </div>
     )
 
+
 }
-
-
 
 
 export default App;
