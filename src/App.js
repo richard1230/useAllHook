14ac2b7a9a7d179f6787b49a8d96dbf9abd4aff7
richard1230 <yongjinhu1230@gmail.com>
2020-12-08 18:12:30 +0800

    diy useMystate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const rootElement = document.getElementById("root");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let _state = [];
+//index是_state这个数组里面的下标,由于这里只用了两个useState，故这里index只能到1
+let index = 0;
+const myUseState = initialValue =>{
+  const currentIndex = index;
+    console.log("此时的 currentIndex为：" +currentIndex);
+    _state[currentIndex] =
+      _state[currentIndex] === undefined ? initialValue : _state[currentIndex];
+  const setState = newValue =>{
+    _state[currentIndex] = newValue;
+    render();
+  }
+    index += 1;
+    console.log("此时的_state为"+_state);
+    // <br/>
+    // console.log("此时的_state长度为"+_state.length);
+    return [_state[currentIndex],setState]
 }
+const render = ()=>{
+    //如果这里不重置为0,那么_state这个数组里面的元素个数就会不断增加
+  index = 0;
+  ReactDOM.render(<App />,rootElement);
+}
+
+const App = ()=>{
+
+  const [n,setN] = myUseState(0);
+  const [m,setM] = myUseState(0);
+  return (
+      <div className="App">
+        <p>此时的n为: {n}</p>
+        <p>
+          <button onClick={()=> setN(n+1)}>+1</button>
+        </p>
+        <p>此时的m为：{m}</p>
+        <p>
+          <button onClick={()=> setM(m+1)}>+1</button>
+        </p>
+      </div>
+  )
+
+
+}
+
+
+
 
 export default App;
