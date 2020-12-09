861103a46442301c2cd63c3c4453b0bcdf434be2
richard1230 <yongjinhu1230@gmail.com>
2020-12-09 13:24:22 +0800

    useRef

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import React, {useState, useReducer, useEffect, useLayoutEffect,useMemo} from 'react';
+import React, {useState, useReducer, useEffect, useLayoutEffect, useMemo, useRef} from 'react';
 import ReactDOM from 'react-dom';
 
 
 const App = () => {
     console.log("app执行了");
+    const count = useRef(0)
     const [n, setN] = React.useState(0);
-    const [m, setM] = React.useState(0);
     const onClick = () => {
-        setN(n + 1);
+        setN(n + 9);
     };
+    useEffect(() => {
+        count.current += 1;
+        console.log(count.current);
+    })
 
-    const onClickChild = useMemo(() => {
-        return ()=>{
-            console.log(m);
-        }
-    },[m])//只有m在变化的时候onClickChild才执行
     return (
         <div className="App">
             <div>
                 <button onClick={onClick}>update n {n}</button>
             </div>
-            <Child2 data={m} onClick={onClickChild}/>
         </div>
     )
 
 }
 
-const Child = (props) => {
-    console.log("child 执行了");
-    console.log('假设这里有大量代码')
-    return <div
-        onClick={props.onclick}
-    >
-        child: {props.data}
-    </div>;
-}
-
-const Child2 = React.memo(Child);
 export default App;
