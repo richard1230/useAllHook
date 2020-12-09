84406fa183d7c283936a045ffd7ac59b3897064f
richard1230 <yongjinhu1230@gmail.com>
2020-12-09 12:14:37 +0800

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, {useState, useReducer, useEffect, useLayoutEffect} from 'react';
+import React, {useState, useReducer, useEffect, useLayoutEffect,useMemo} from 'react';
 import ReactDOM from 'react-dom';
 
 
 const App = () => {
+    console.log("app执行了");
     const [n, setN] = React.useState(0);
     const [m, setM] = React.useState(0);
     const onClick = () => {
         setN(n + 1);
     };
-    const onClick2 = () => {
-        setM(m=>m + 1);
-    };
+
+    const onClickChild = useMemo(() => {
+        return ()=>{
+            console.log(m);
+        }
+    },[m])//只有m在变化的时候onClickChild才执行
     return (
         <div className="App">
             <div>
                 <button onClick={onClick}>update n {n}</button>
-                <button onClick={onClick2}>update m {m}</button>
-
             </div>
-            {/*<Child data={m}/>*/}
-             <Child2 data={m}/>
+            <Child2 data={m} onClick={onClickChild}/>
         </div>
     )
 
@@ -28,8 +29,11 @@ const App = () => {
 const Child = (props) => {
     console.log("child 执行了");
     console.log('假设这里有大量代码')
-    return <div>child: {props.data}</div>;
-
+    return <div
+        onClick={props.onclick}
+    >
+        child: {props.data}
+    </div>;
 }
 
 const Child2 = React.memo(Child);
