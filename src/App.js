d934e25cf20209cef6775b3c869a817f0c849c92
richard1230 <yongjinhu1230@gmail.com>
2020-12-09 11:04:13 +0800

    useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React, {useState, useReducer,useEffect} from 'react';
+import React, {useState, useReducer, useEffect,useLayoutEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 
-
 const App = () => {
-    const [n,setN]= useState(0);
-    const onClick = ()=>{
-        setN(i=>i+1)
+    const [n, setN] = useState(0);
+    const onClick = () => {
+        setN(i => i + 1)
     }
-    useEffect(()=>{
+    useEffect(() => {
         console.log("第一次渲染之后就执行");
-    },[])//[]里面的的变量变化时就执行 => 不会执行
+    }, [])//[]里面的的变量变化时就执行 => 不会执行
 
-    useEffect(()=>{
-        if (n !== 0){
-            console.log("n变化了");
-        }
-    },[n])//n变化的时候才执行
+    useLayoutEffect(() => {
+        console.log("这里是执行useLayoutEffect1");
+    })//n变化的时候才执行
 
-    useEffect(()=>{
-        console.log("无论何时变化都执行");
+    useLayoutEffect(() => {
+        console.log("这里是执行useLayoutEffect2");
     });
-    return(
+    return (
         <div>
             n:{n}
             <button onClick={onClick}>+1</button>
