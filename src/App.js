2ed121deb533fe0fe027752efbbedd52ab19ad1b
richard1230 <yongjinhu1230@gmail.com>
2020-12-09 11:19:43 +0800

    多余的render

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, {useState, useReducer, useEffect,useLayoutEffect} from 'react';
+import React, {useState, useReducer, useEffect, useLayoutEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 
 const App = () => {
-    const [n, setN] = useState(0);
+    const [n, setN] = React.useState(0);
+    const [m, setM] = React.useState(0);
     const onClick = () => {
-        setN(i => i + 1)
-    }
-    useEffect(() => {
-        console.log("第一次渲染之后就执行");
-    }, [])//[]里面的的变量变化时就执行 => 不会执行
-
-    useLayoutEffect(() => {
-        console.log("这里是执行useLayoutEffect1");
-    })//n变化的时候才执行
-
-    useLayoutEffect(() => {
-        console.log("这里是执行useLayoutEffect2");
-    });
+        setN(n + 1);
+    };
     return (
-        <div>
-            n:{n}
-            <button onClick={onClick}>+1</button>
+        <div className="App">
+            <div>
+                <button onClick={onClick}>update n {n}</button>
+            </div>
+            <Child data={m}/>
+             {/*<Child2 data={m}/> */}
         </div>
     )
 
 }
 
+const Child = (props) => {
+    console.log("child 执行了");
+    console.log('假设这里有大量代码')
+    return <div>child: {props.data}</div>;
 
+}
+
+const Child2 = React.memo(Child);
 export default App;
