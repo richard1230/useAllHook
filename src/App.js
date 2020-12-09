4b8b88f35f431dc4fbc4e1262d3e15aa48d679a8
richard1230 <yongjinhu1230@gmail.com>
2020-12-09 10:28:41 +0800

    useEffect 1

--- src/App.js
+++ src/App.js
@@ -1,74 +1,33 @@
-import React, {useState, useReducer} from 'react';
+import React, {useState, useReducer,useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
-//1.创建初始值
-const initialFormData = {
-    name: "",
-    age: 18,
-    nationality: "汉族"
-}
 
-//2.创建所有操作
-const reducer = (state, action) => {
-    switch (action.type) {
-        case "patch":
-            return {...state, ...action.formData};
-        case "reset":
-            return initialFormData;
-        default:
-            throw new Error();
-    }
-}
 
 const App = () => {
-    //3.state保存初始状态，可以读；dispatch可以写
-    const [formData, dispatch] = useReducer(reducer, initialFormData)
-
-    const onReset = () => {
-        dispatch({type: 'reset'})
+    const [n,setN]= useState(0);
+    const onClick = ()=>{
+        setN(i=>i+1)
     }
-    const onSubmit = () => {
-    }
+    useEffect(()=>{
+        console.log("第一次渲染之后就执行");
+    },[])//[]里面的的变量变化时就执行 => 不会执行
 
-    return (
-        <form onSubmit={onSubmit} onReset={onReset}>
-            <div>
-                <label>
-                    姓名
-                    <input
-                        value={formData.name}
-                        onChange={e =>
-                            dispatch({type: "patch", formData: {name: e.target.value}})
-                        }
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    年龄
-                    <input
-                        value={formData.age}
-                        onChange={e => dispatch({type: "patch", formData: {age: e.target.value}})}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    民族
-                    <input
-                        value={formData.nationality}
-                        onChange={e => dispatch({type: "patch", formData: {nationality: e.target.value}})}
-                    />
-                </label>
-            </div>
-            <div>
-                <button type="submit">提交</button>
-                <button type="reset">重置</button>
-            </div>
-            <hr/>
-            {JSON.stringify(formData)}
-        </form>
-    );
+    useEffect(()=>{
+        if (n !== 0){
+            console.log("n变化了");
+        }
+    },[n])//n变化的时候才执行
+
+    useEffect(()=>{
+        console.log("无论何时变化都执行");
+    });
+    return(
+        <div>
+            n:{n}
+            <button onClick={onClick}>+1</button>
+        </div>
+    )
+
 }
 
 
